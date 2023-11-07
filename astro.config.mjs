2ea92398507a4b22942042468832b023b6b0686f
guom <2217882249@qq.com>
2023-11-07 18:03:08 +0800

    add base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gm424.github.io',
+  base: 'AstroProject',
   integrations: [
     vue({
       appEntrypoint: '/src/id-provider',
