176819bfcdf0b4b4550e5dbddb4d027a84834b52
zau11berer <zau11berer@outlook.com>
2023-11-09 10:35:43 +0800

    增加ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gm424.github.io',
-  base: 'AstroProject',
   output: 'server',
-
   integrations: [
     vue({
       appEntrypoint: '/src/id-provider',
     }),
   ],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
