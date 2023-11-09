37ac47853e5ec854dae0ddc598b779cd84503011
guom <2217882249@qq.com>
2023-11-09 16:16:18 +0800

    update the astro base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gm424.github.io',
+  base: 'AstroProject',
   output: 'server',
   integrations: [
     vue({
